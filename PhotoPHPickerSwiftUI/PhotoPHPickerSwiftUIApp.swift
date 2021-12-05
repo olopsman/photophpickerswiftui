5519438a5cba339ece477c4a408804ceaed31369
Paulo Orquillo <paulo.orquillo@gmail.com>
2021-12-05 19:42:59 +1300

    Initial Commit

---
+++ PhotoPHPickerSwiftUI/PhotoPHPickerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPHPickerSwiftUIApp.swift
+//  PhotoPHPickerSwiftUI
+//
+//  Created by Paulo Orquillo on 5/12/21.
+//
+
+import SwiftUI
+
+@main
+struct PhotoPHPickerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
